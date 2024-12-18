0d2d1725f36ed14dc1f002c2cdf2a42cb717704e
Zhandos <komaedan@yandex.ru>
2024-12-18 23:59:49 +0500

    generative id

--- src/main/java/com/test/model/Book.java
+++ src/main/java/com/test/model/Book.java
@@ -2,16 +2,13 @@ package com.test.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
 public class Book {
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
